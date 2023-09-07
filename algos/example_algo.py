049537d09bfecfee13aff24a87aaa735966f50b1
Philipp Drebes <philipp.drebes@gmail.com>
2023-09-07 16:34:44 +0200

    change example algo

--- algos/example_algo.py
+++ algos/example_algo.py
@@ -1,7 +1,4 @@
-import pandas as pd
-import numpy as np
 import os
-import time
 import json
 
 
@@ -36,25 +33,29 @@ def get_job_details():
     return job
 
 
-def line_counter(job_details):
-    """Executes the line counter based on inputs"""
-    print('Starting compute job with the following input information:')
-    print(json.dumps(job_details, sort_keys=True, indent=4))
-
-    """ Now, count the lines of the first file in first did """
-    first_did = job_details['dids'][0]
-    filename = job_details['files'][first_did][0]
-    non_blank_count = 0
-    with open(filename) as infp:
-        for line in infp:
-            if line.strip():
-                non_blank_count += 1
-    print('number of non-blank lines found %d' % non_blank_count)
-    """ Print that number to output to generate algo output"""
-    f = open("/data/outputs/result", "w")
-    f.write(str(non_blank_count))
-    f.close()
+# def line_counter(job_details):
+#     """Executes the line counter based on inputs"""
+#     print('Starting compute job with the following input information:')
+#     print(json.dumps(job_details, sort_keys=True, indent=4))
+#
+#     """ Now, count the lines of the first file in first did """
+#     first_did = job_details['dids'][0]
+#     filename = job_details['files'][first_did][0]
+#     non_blank_count = 0
+#     with open(filename) as infp:
+#         for line in infp:
+#             if line.strip():
+#                 non_blank_count += 1
+#     print('number of non-blank lines found %d' % non_blank_count)
+#     """ Print that number to output to generate algo output"""
+#     f = open("/data/outputs/result", "w")
+#     f.write(str(non_blank_count))
+#     f.close()
 
 
 if __name__ == '__main__':
-    line_counter(get_job_details())
+    print('Starting algo')
+    f = open("/data/outputs/result", "w")
+    f.write("Hello World")
+    f.close()
+    # line_counter(get_job_details())
